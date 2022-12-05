0b76f510bba8a45e8e7db6c8686061bbbf66b246
choi chiwon <choichiwon@chois-MacBook-Pro.local>
2022-12-05 10:34:59 +0900

    #01 electronmon

---
+++ README.md
@@ -0,0 +1,9 @@
+# Image Resizer with Electron
+
+### #01 electronmon
+
+-nodemon과 동일
+
+```bash
+npx electronmon .
+```
---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = process.env.NODE_ENV !== "production";
+const isMac = process.platform === "darwin";
+
+const createMainWindow = () => {
+  const win = new BrowserWindow({
+    title: "Image Resizer",
+    width: isDev ? 1000 : 500,
+    height: 700,
+  });
+
+  if (isDev) win.webContents.openDevTools();
+
+  win.loadFile(path.join(__dirname, "./renderer/index.html"));
+};
+
+app.whenReady().then(() => {
+  createMainWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
